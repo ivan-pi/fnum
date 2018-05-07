8e21d69febfc360354a53c0b3a56647101a43545
Ivan <ivan.pribec@gmail.com>
2018-05-07 18:15:23 +0200

    added tau constant

--- src/math_mod.f90
+++ src/math_mod.f90
@@ -5,14 +5,20 @@ module math_mod
     implicit none
     private
 
-    public :: pi, pi_2, pi_4, pi_8
+    public :: one, two
+    public :: pi, pi_2, pi_4, pi_8, tau
     public :: euler
     public :: golden, golden_ratio
+
+    real(wp), parameter :: one = 1.0_wp
+    real(wp), parameter :: two = 2.0_wp
 
     real(wp), parameter :: pi = 4._wp*atan(1.0_wp)
     real(wp), parameter :: pi_2 = 0.5_wp*pi
     real(wp), parameter :: pi_4 = 0.25_wp*pi
     real(wp), parameter :: pi_8 = 0.125_wp*pi
+
+    real(wp), parameter :: tau = 2*pi ! https://www.youtube.com/watch?v=WywQ6a3uQ5I&t=1430s
 
     real(wp), parameter :: euler = exp(1.0_wp)
 
